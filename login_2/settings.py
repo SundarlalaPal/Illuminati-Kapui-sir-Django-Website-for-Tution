de31989f852442b1dbb52a7aa14600a33a39324b
SundarlalaPal <134628190+SundarlalaPal@users.noreply.github.com>
2025-04-12 08:08:31 +0530

    Update settings.py

--- login_2/settings.py
+++ login_2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
